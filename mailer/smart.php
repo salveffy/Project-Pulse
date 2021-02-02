d439c4bd5fd854111d2f168980ee81f56b8b8e40
salveffy <36531190+salveffy@users.noreply.github.com>
2021-02-02 13:03:04 +0300

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -13,8 +13,8 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'faretthan@yandex.ru';                 // Наш логин
-$mail->Password = 'f9m3c8j0v1s4l7';                           // Наш пароль от ящика
+$mail->Username = '';                 // Наш логин
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
